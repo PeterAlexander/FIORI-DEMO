73415b501d665d457487439644e4bbb648d2c6f0
SAP Education Course CP-A <cp-a@education.cloud.sap>
2020-04-17 08:30:56 +0000

    initial

---
+++ webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"fiori/demo/app/FIORI-DEMO/test/unit/AllTests"
+	], function () {
+		QUnit.start();
+	});
+});
